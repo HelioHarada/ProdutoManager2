fe42f034b0b763ac7d7925cb826951325cac21e1
Harada <helioharada30@gmail.com>
2018-03-01 21:35:47 -0300

    vai

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -25,10 +25,11 @@ namespace ProdutoManager.Controllers
         }
 
         // GET: Produto/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int Id)
         {
-            ListaDeProduto.Where(x => x.Id == id);
-            return View();
+         var produto = ListaDeProdutos.FirstOrDefault(x => x.Id == Id);
+
+         return View(produto);
         }
 
         // GET: Produto/Create
@@ -54,19 +55,50 @@ namespace ProdutoManager.Controllers
         }
 
         // GET: Produto/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = ListaDeProdutos.FirstOrDefault(m => m.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
         }
 
         // POST: Produto/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, [Bind("Id", "Nome", "Categoria", "Preco")] Produto produto)
         {
             try
             {
                 // TODO: Add update logic here
+
+                if (id != produto.Id)
+                {
+                    return NotFound();
+                }
+                if (ModelState.IsValid)
+                {
+                    var ProdutoTemp = ListaDeProdutos.FirstOrDefault(c => c.Id == id);
+                    if (ProdutoTemp != null)
+                    {
+                        ProdutoTemp.Nome = produto.Nome;
+                        ProdutoTemp.Categoria = produto.Categoria;
+                        ProdutoTemp.Preco = produto.Preco;
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
 
                 return RedirectToAction(nameof(Index));
             }
@@ -77,19 +109,32 @@ namespace ProdutoManager.Controllers
         }
 
         // GET: Produto/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = ListaDeProdutos.FirstOrDefault(y => y.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
         }
 
         // POST: Produto/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteConfirm(int id)
         {
             try
             {
                 // TODO: Add delete logic here
+                var produto = ListaDeProdutos.FirstOrDefault(y => y.Id == id);
+                ListaDeProdutos.Remove(produto);
 
                 return RedirectToAction(nameof(Index));
             }
--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -12,9 +12,9 @@ namespace ProdutoManager.Controllers
     {
         static List<Usuario> ListaDeUsuario = new List<Usuario>
         {
-            new Usuario{Id=1, Nome="Helinho", Cpf= "242.424.242-4", Email="helinhogatinhonaruto@hotmail.com"},
-            new Usuario{Id=2, Nome="Guilherminho Falcão", Cpf= "242.424.242-4", Email="guilherminhodaquebrada@hotmail.com"},
-            new Usuario{Id=3, Nome="Leozinhoh", Cpf= "123.456.789-1", Email="bichosoltodequebradabelga@hotmail.com"}
+            new Usuario{Id=1, Nome="Helio", Cpf= "242.424.242-4", Email="helioo@hotmail.com"},
+            new Usuario{Id=2, Nome="Tiago", Cpf= "242.424.242-4", Email="guilherminhodaquebrada@hotmail.com"},
+            new Usuario{Id=3, Nome="Leozin", Cpf= "123.456.789-1", Email="bichosoltodequebradabelga@hotmail.com"}
         };
 
         // GET: Usuario
@@ -26,8 +26,10 @@ namespace ProdutoManager.Controllers
         // GET: Usuario/Details/5
         public ActionResult Details(int id)
         {
-            ListaDeUsuario.Where(x => x.Id == id);
-            return View();
+
+            var usuario = ListaDeUsuario.FirstOrDefault(x => x.Id == id);
+
+            return View(usuario);
         }
 
         // GET: Usuario/Create
@@ -54,19 +56,49 @@ namespace ProdutoManager.Controllers
         }
 
         // GET: Usuario/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = ListaDeUsuario.FirstOrDefault(m => m.Id == id);
+            if(usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
         }
 
         // POST: Usuario/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, [Bind("Id","Nome","Cpf","Email")] Usuario usuario)
         {
             try
             {
                 // TODO: Add update logic here
+                
+                if (id != usuario.Id)
+                {
+                    return NotFound();
+                }
+                if(ModelState.IsValid)
+                {
+                    var UsuarioTemp = ListaDeUsuario.FirstOrDefault(c => c.Id == id);
+                    if(UsuarioTemp != null)
+                    {
+                        UsuarioTemp.Nome = usuario.Nome;
+                        UsuarioTemp.Cpf = usuario.Cpf;
+                        UsuarioTemp.Email = usuario.Email;
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
 
                 return RedirectToAction(nameof(Index));
             }
@@ -77,19 +109,32 @@ namespace ProdutoManager.Controllers
         }
 
         // GET: Usuario/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = ListaDeUsuario.FirstOrDefault(y => y.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
         }
 
         // POST: Usuario/Delete/5
-        [HttpPost]
+        [HttpPost,ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult DeleteConfirm(int id)
         {
             try
             {
                 // TODO: Add delete logic here
+                var usuario = ListaDeUsuario.FirstOrDefault(m => m.Id == id);
+                ListaDeUsuario.Remove(usuario);
 
                 return RedirectToAction(nameof(Index));
             }
